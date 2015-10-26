d2b775b0383aed2ff8435d16cab2b057a0c48343
antoine <aspegicus@gmail.com>
2015-10-26 20:31:32 +0100

    Initial setup - trying Solved by Flexbox theme

---
+++ sites/all/themes/laliste/template.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Main template file for laliste theme
+ *
+**/
+
+function laliste_css_alter(&$css) {
+    unset($css[drupal_get_path('module','system').'/system.theme.css']);
+    unset($css[drupal_get_path('module','system').'/system.base.css']);
+    unset($css[drupal_get_path('module','system').'/system.menus.css']);
+}
