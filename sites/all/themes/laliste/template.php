9287fcd0265717e16cd7b744ee737c12ab931b98
antoine <aspegicus@gmail.com>
2015-10-27 16:48:57 +0100

    Removing default css files bloating the site

--- sites/all/themes/laliste/template.php
+++ sites/all/themes/laliste/template.php
@@ -5,6 +5,9 @@
  *
 **/
 
+/*
+ * Removing unecassry css files bloating our site
+ **/
 function laliste_css_alter(&$css) {
     unset($css[drupal_get_path('module','system').'/system.theme.css']);
     unset($css[drupal_get_path('module','system').'/system.base.css']);
@@ -14,4 +17,8 @@ function laliste_css_alter(&$css) {
     unset($css[drupal_get_path('module','field').'/theme/field.css']);
     unset($css[drupal_get_path('module','search').'/search.css']);
     unset($css[drupal_get_path('module','user').'/user.css']);
+    unset($css[drupal_get_path('module','node').'/node.css']);
+    unset($css[drupal_get_path('module','views').'/css/views.css']);
+    unset($css[drupal_get_path('module','ctools').'/css/ctools.css']);
+    unset($css[drupal_get_path('module','addressfield').'/addressfield.css']);
 }
