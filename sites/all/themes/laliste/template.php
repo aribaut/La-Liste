62d57ef507a90916e454b7629e90857d84cb960e
antoine <aspegicus@gmail.com>
2015-10-27 17:04:25 +0100

    Excluding admin system css

--- sites/all/themes/laliste/template.php
+++ sites/all/themes/laliste/template.php
@@ -13,6 +13,7 @@ function laliste_css_alter(&$css) {
     unset($css[drupal_get_path('module','system').'/system.base.css']);
     unset($css[drupal_get_path('module','system').'/system.menus.css']);
     unset($css[drupal_get_path('module','system').'/system.messages.css']);
+    unset($css[drupal_get_path('module','system').'/system.admin.css']);
     unset($css[drupal_get_path('module','comment').'/comment.css']);
     unset($css[drupal_get_path('module','field').'/theme/field.css']);
     unset($css[drupal_get_path('module','search').'/search.css']);
