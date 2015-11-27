2031bb3f2242b64e0b971670e285f3e45082825a
antoine <aspegicus@gmail.com>
2015-11-27 09:00:08 +0100

    Added flexslider and views associated module

--- sites/all/themes/laliste/templates/page--front.tpl.php
+++ sites/all/themes/laliste/templates/page--front.tpl.php
@@ -80,7 +80,7 @@
     <?php print render($page['header']); ?>
   </div>
 </header>
-<main class="content-body">
+<main class="content-body-home">
   <div class="content-container">
     <?php //print render($title_prefix); ?>
     <?php if (false): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
