8b8e9dd15e99971c33f48394a584ba728a19216a
antoine <aspegicus@gmail.com>
2015-12-17 00:39:11 +0100

    fix translations

--- sites/all/themes/laliste/templates/page--front.tpl.php
+++ sites/all/themes/laliste/templates/page--front.tpl.php
@@ -114,13 +114,13 @@
 <footer class="footer-main">
   <div class="footer-container">
   <?php //print render($page['footer']); ?>
-    <a class="footer-link" href="/about"><?php print t('ABOUT'); ?></a>
-    <a class="footer-link" href="/our-team"><?php print t('TEAM'); ?></a>
+    <a class="footer-link" href="about"><?php print t('ABOUT'); ?></a>
+    <a class="footer-link" href="our-team"><?php print t('TEAM'); ?></a>
     <a class="footer-link" href="mailto:contact@laliste.com?subject=Contact%20from%20laliste.com"><?php print t('CONTACT'); ?></a>
-    <a class="footer-link" href="#"><?php print t('FAQ'); ?></a>
+    <a class="footer-link" href="news"><?php print t('NEWS'); ?></a>
   </div>
   <div class="sub-footer">
-    <a class="sub-footer-link" href="/legal-notice"><?php print t('LEGAL NOTICE'); ?></a>
-    <a class="sub-footer-link" href="/conditions-of-use"><?php print t('CONDITIONS OF USE'); ?></a>
+    <a class="sub-footer-link" href="legal-notice"><?php print t('LEGAL NOTICE'); ?></a>
+    <a class="sub-footer-link" href="conditions-of-use"><?php print t('CONDITIONS OF USE'); ?></a>
   </div>
 </footer>
